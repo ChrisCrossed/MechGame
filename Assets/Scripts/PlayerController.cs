1d181267aea2d4b7493f868beb931e9b6b03851b
ChrisCrossed <chriscrossed.86@gmail.com>
2025-11-30 09:46:39 -0700

    Fixing my mistake with Visual Studio

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,4 +16,77 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    #region Collider Extensions
+
+    public LayerMask obstacleLayers;
+    public Color mtvColor = Color.yellow;
+    public bool autoResolve = true;
+    public bool smoothResolve = true;
+
+    public event Action<Vector3> OnPenetrationStart;
+    public event Action<Vector3> OnPenetrationStay;
+    public event System.Action OnPenetrationEnd;
+
+    Collider col;
+    Vector3 lastCorrection;
+    bool resolvingCollision;
+
+    void ObjectClipStart()
+    {
+        col = GetComponent<Collider>();
+
+        OnPenetrationStart += correction =>
+        {
+            float penetrationDepth = correction.magnitude;
+        };
+    }
+
+    void ObjectClipUpdate()
+    {
+        bool colliding = col.GetPenetrationsInLater(obstacleLayers, out Vector3 correction);
+        correction += correction.normalized * 0.001f;
+        lastCorrection = colliding ? correction : Vector3.zero;
+
+        if (colliding)
+        {
+            if (!resolvingCollision) OnPenetrationStart?.Invoke(correction);
+            else OnPenetrationStay?.Invoke(correction);
+
+            resolvingCollision = true;
+
+            if (autoResolve)
+            {
+                Vector3 delta = smoothResolve
+                    ? Vector3.Lerp(Vector3.zero, correction, 0.05f)
+                    : correction;
+
+                transform.position += delta;
+            }
+
+            Debug.Log($"Colliding, MTV = {correction.magnitude:F3}");
+        }
+        else
+        {
+            if (resolvingCollision) OnPenetrationEnd?.Invoke();
+            resolvingCollision = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (col == null) col = GetComponent<Collider>();
+        if (col == null) return;
+
+        if (lastCorrection != Vector3.zero)
+        {
+            Vector3 start = col.bounds.center;
+            Vector3 end = start + lastCorrection;
+            Gizmos.color = mtvColor;
+            Gizmos.DrawLine(start, end);
+            Gizmos.DrawSphere(end, 0.05f);
+        }
+    }
+
+    #endregion
 }
