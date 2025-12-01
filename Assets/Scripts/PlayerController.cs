4773ba79085f79256aad07c27a5268a237f87260
ChrisCrossed <chriscrossed.86@gmail.com>
2025-11-30 23:02:10 -0700

    Fixing Unity Bugs
    Fixing PlayerController input applying velocity changes
    Fixing Unity not choosing to update scripts after modifying them

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,183 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private PlayerInputObject PlayerInput;
+    private PlayerInputObject PlayerInput_OLD;
+
+    private Rigidbody this_Rigidbody;
+
+    private bool PlayerJump;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Chris Test
+        print("Start");
+        INIT_PlayerInput();
+        INIT_PlayerObjectComponents();
     }
+
+    #region Init Functions
+    void INIT_PlayerInput()
+    {
+        PlayerInput = new PlayerInputObject();
+
+        PlayerInput.Init();
+    }
+
+    void INIT_PlayerObjectComponents()
+    {
+        this_Rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    #endregion Init Functions
 
     // Update is called once per frame
     void Update()
     {
-        
+        // UPDATE_Input();
+        TempInput();
     }
+
+    #region Update Functions
+
+    private PlayerInputObject UPDATE_Input()
+    {
+        PlayerInput_OLD = PlayerInput;
+
+        PlayerInput.InputUpdate();
+
+        #region Jump Evaluation
+        PlayerJump = false;
+
+        if (PlayerInput.Jump && !PlayerInput_OLD.Jump)
+            PlayerJump = true;
+        #endregion Jump Evaluation
+
+        print(PlayerInput.MoveVector);
+
+        return PlayerInput;
+    }
+
+    #endregion Update Functions
+
+    #region Physics Updates
+
+    Rigidbody rigidbody_OLD;
+    private void FixedUpdate()
+    {
+        rigidbody_OLD = this_Rigidbody;
+
+        FIXEDUPDATE_PlayerMovement();
+    }
+    float val = 0f;
+    Vector3 v3_OldVelocity;
+    private void FIXEDUPDATE_PlayerMovement()
+    {
+        // Vector3 v3_OldVelocity = rigidbody_OLD.linearVelocity;
+        Vector3 v3_OldVelocity_Normalized = v3_OldVelocity.normalized;
+        Vector3 v3_NewVelocity = v3_OldVelocity;
+
+        print("Input: " + v2_Input);
+
+        if(v2_Input.x != 0f)
+        {
+            if(v2_Input.x > 0f)
+            {
+                // Opposite sign of input gets most velocity change
+                if (v3_OldVelocity.x < 0f)
+                {
+                    v3_NewVelocity.x += Time.fixedDeltaTime * 5f;
+                }
+                else
+                {
+                    v3_NewVelocity.x += Time.fixedDeltaTime * 3f;
+                }
+            }
+            else if(v2_Input.x < 0f)
+            {
+                // Opposite sign of input gets most velocity change
+                if(v3_OldVelocity.x > 0f)
+                {
+                    v3_NewVelocity.x -= Time.fixedDeltaTime * 5f;
+                }
+                else
+                {
+                    v3_NewVelocity.x -= Time.fixedDeltaTime * 3f;
+                }
+            }
+        }
+        else
+        {
+            if(v3_NewVelocity.x < 0f)
+            {
+                v3_NewVelocity.x += Time.fixedDeltaTime * 3f;
+                if (v3_OldVelocity.x > 0f)
+                    v3_NewVelocity.x = 0f;
+            }
+            else
+            {
+                v3_NewVelocity.x -= Time.fixedDeltaTime * 3f;
+                if (v3_OldVelocity.x < 0f)
+                    v3_NewVelocity.x = 0f;
+            }
+        }
+
+        v3_NewVelocity.x = Mathf.Clamp(v3_NewVelocity.x, -10f, 10f);
+
+        print("Vel: " + v3_NewVelocity.x);
+
+        v3_OldVelocity = v3_NewVelocity;
+        /*
+        if (PlayerInput.MoveVector.Y == 1f)
+        {
+            print("True");
+            this_Rigidbody.linearVelocity = gameObject.transform.rotation * new Vector3(0f, 5f, 0f);
+        }
+        */
+    }
+
+
+    #endregion Physics Updates
+
+    #region TEMP INPUT
+
+    Vector2 v2_Input;
+    bool JumpButton;
+    bool JumpButton_OLD;
+    bool JumpPressed;
+    void TempInput()
+    {
+        #region Input
+        v2_Input = new Vector2();
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        bool rightPressed = Input.GetKey(KeyCode.D);
+        bool upPressed = Input.GetKey(KeyCode.W);
+        bool downPressed = Input.GetKey(KeyCode.S);
+
+        if (leftPressed && !rightPressed)
+            v2_Input.x = -1f;
+        else if(rightPressed && !leftPressed)
+            v2_Input.x = 1f;
+
+        if(upPressed && !downPressed)
+            v2_Input.y = 1f;
+        else if(downPressed && !upPressed)
+            v2_Input.y = -1f;
+
+        v2_Input.Normalize();
+        #endregion Input
+
+        #region Jump
+        JumpButton_OLD = JumpButton;
+        JumpButton = false;
+        if (Input.GetKey(KeyCode.Space))
+            JumpButton = true;
+
+        JumpPressed = JumpButton && !JumpButton_OLD;
+        #endregion Jump
+    }
+    #endregion
 
     #region Collider Extensions
 
